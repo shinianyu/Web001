92a45d48d6c1b3213f3eaa71aaece06d553db47c
李建峰 <good365kxsh@qq.com>
2020-02-11 19:58:06 +0800

    Initial commit

---
+++ src/com/atguigu/helloTest.java
@@ -0,0 +1,18 @@
+package com.atguigu;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class helloTest {
+    public static void main(String[] args) {
+        List list = new ArrayList();
+        ArrayList arrayList = new ArrayList();
+    }
+    public void a(){
+
+    }
+    public void b(){
+        
+    }
+}
